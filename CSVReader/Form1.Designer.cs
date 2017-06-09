a1bbdd5186c097ae0632b8c6dffed17f3e87524a
Paul Scott <paul.scott.kildare@live.com>
2017-06-09 17:44:01 +0100

    Add files

---
+++ CSVReader/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace CSVReader {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+			this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+			this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolsOptionMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.closeButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.openButton = new System.Windows.Forms.ToolStripButton();
+			this.saveButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.csvgrid = new System.Windows.Forms.DataGridView();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.statustext = new System.Windows.Forms.ToolStripStatusLabel();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.headerButton = new System.Windows.Forms.ToolStripMenuItem();
+			this.headerOptionMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.csvgrid)).BeginInit();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(663, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.toolStripSeparator,
+            this.saveMenuItem,
+            this.saveAsMenuItem,
+            this.toolStripSeparator2,
+            this.exitMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// openMenuItem
+			// 
+			this.openMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openMenuItem.Image")));
+			this.openMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openMenuItem.Name = "openMenuItem";
+			this.openMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openMenuItem.Text = "&Open";
+			this.openMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
+			// 
+			// toolStripSeparator
+			// 
+			this.toolStripSeparator.Name = "toolStripSeparator";
+			this.toolStripSeparator.Size = new System.Drawing.Size(149, 6);
+			// 
+			// saveMenuItem
+			// 
+			this.saveMenuItem.Enabled = false;
+			this.saveMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveMenuItem.Image")));
+			this.saveMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.saveMenuItem.Name = "saveMenuItem";
+			this.saveMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveMenuItem.Text = "&Save";
+			// 
+			// saveAsMenuItem
+			// 
+			this.saveAsMenuItem.Enabled = false;
+			this.saveAsMenuItem.Name = "saveAsMenuItem";
+			this.saveAsMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveAsMenuItem.Text = "Save &As";
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+			// 
+			// exitMenuItem
+			// 
+			this.exitMenuItem.Name = "exitMenuItem";
+			this.exitMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitMenuItem.Text = "E&xit";
+			this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+			// 
+			// editToolStripMenuItem
+			// 
+			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.selectAllToolStripMenuItem});
+			this.editToolStripMenuItem.Enabled = false;
+			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+			this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+			this.editToolStripMenuItem.Text = "&Edit";
+			// 
+			// cutToolStripMenuItem
+			// 
+			this.cutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("cutToolStripMenuItem.Image")));
+			this.cutToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+			this.cutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+			this.cutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.cutToolStripMenuItem.Text = "Cu&t";
+			// 
+			// copyToolStripMenuItem
+			// 
+			this.copyToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripMenuItem.Image")));
+			this.copyToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+			this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+			this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.copyToolStripMenuItem.Text = "&Copy";
+			// 
+			// pasteToolStripMenuItem
+			// 
+			this.pasteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("pasteToolStripMenuItem.Image")));
+			this.pasteToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+			this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+			this.pasteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.pasteToolStripMenuItem.Text = "&Paste";
+			// 
+			// toolStripSeparator4
+			// 
+			this.toolStripSeparator4.Name = "toolStripSeparator4";
+			this.toolStripSeparator4.Size = new System.Drawing.Size(149, 6);
+			// 
+			// selectAllToolStripMenuItem
+			// 
+			this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+			this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.selectAllToolStripMenuItem.Text = "Select &All";
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsOptionMenuItem});
+			this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+			this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+			this.toolsToolStripMenuItem.Text = "&Tools";
+			// 
+			// toolsOptionMenuItem
+			// 
+			this.toolsOptionMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.headerOptionMenuItem});
+			this.toolsOptionMenuItem.Name = "toolsOptionMenuItem";
+			this.toolsOptionMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.toolsOptionMenuItem.Text = "&Options";
+			this.toolsOptionMenuItem.Click += new System.EventHandler(this.headerButton_Click_1);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+			this.aboutToolStripMenuItem.Text = "&About...";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeButton,
+            this.toolStripSeparator1,
+            this.openButton,
+            this.saveButton,
+            this.toolStripSeparator3,
+            this.toolStripDropDownButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(663, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// closeButton
+			// 
+			this.closeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.closeButton.Image = ((System.Drawing.Image)(resources.GetObject("closeButton.Image")));
+			this.closeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.closeButton.Name = "closeButton";
+			this.closeButton.Size = new System.Drawing.Size(23, 22);
+			this.closeButton.Text = "toolStripButton3";
+			this.closeButton.ToolTipText = "Close";
+			this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+			// 
+			// openButton
+			// 
+			this.openButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+			this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openButton.Name = "openButton";
+			this.openButton.Size = new System.Drawing.Size(23, 22);
+			this.openButton.Text = "toolStripButton1";
+			this.openButton.ToolTipText = "Open File";
+			this.openButton.Click += new System.EventHandler(this.openMenuItem_Click);
+			// 
+			// saveButton
+			// 
+			this.saveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.saveButton.Enabled = false;
+			this.saveButton.Image = ((System.Drawing.Image)(resources.GetObject("saveButton.Image")));
+			this.saveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(23, 22);
+			this.saveButton.Text = "toolStripButton2";
+			this.saveButton.ToolTipText = "Save File";
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+			// 
+			// csvgrid
+			// 
+			this.csvgrid.AllowDrop = true;
+			this.csvgrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.csvgrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+			this.csvgrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.csvgrid.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.csvgrid.Location = new System.Drawing.Point(0, 49);
+			this.csvgrid.Name = "csvgrid";
+			this.csvgrid.Size = new System.Drawing.Size(663, 341);
+			this.csvgrid.TabIndex = 2;
+			this.csvgrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.csvgrid_CellValueChanged);
+			this.csvgrid.DragDrop += new System.Windows.Forms.DragEventHandler(this.csvgrid_DragDrop);
+			this.csvgrid.DragEnter += new System.Windows.Forms.DragEventHandler(this.csvgrid_DragEnter);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statustext});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 390);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(663, 22);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// statustext
+			// 
+			this.statustext.Name = "statustext";
+			this.statustext.Size = new System.Drawing.Size(0, 17);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "CSV File|*.csv|All files|*.*";
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.headerButton});
+			this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+			this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.ToolTipText = "Options";
+			// 
+			// headerButton
+			// 
+			this.headerButton.Checked = true;
+			this.headerButton.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.headerButton.Name = "headerButton";
+			this.headerButton.Size = new System.Drawing.Size(197, 22);
+			this.headerButton.Text = "Use first row as headers";
+			this.headerButton.Click += new System.EventHandler(this.headerButton_Click_1);
+			// 
+			// headerOptionMenuItem
+			// 
+			this.headerOptionMenuItem.Checked = true;
+			this.headerOptionMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.headerOptionMenuItem.Name = "headerOptionMenuItem";
+			this.headerOptionMenuItem.Size = new System.Drawing.Size(197, 22);
+			this.headerOptionMenuItem.Text = "Use first row as headers";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+			this.ClientSize = new System.Drawing.Size(663, 412);
+			this.Controls.Add(this.csvgrid);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "CSV Reader";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.csvgrid)).EndInit();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+		private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+		private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolsOptionMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton openButton;
+		private System.Windows.Forms.DataGridView csvgrid;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel statustext;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ToolStripButton saveButton;
+		private System.Windows.Forms.ToolStripButton closeButton;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+		private System.Windows.Forms.ToolStripMenuItem headerButton;
+		private System.Windows.Forms.ToolStripMenuItem headerOptionMenuItem;
+	}
+}
+
---
+++ CSVReader/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVReader {
+	public partial class Form1 : Form {
+		private string filename = "";
+		private bool gridDirty = false;
+		private bool firstRowHeader = true;
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			
+		}
+
+		private void exitMenuItem_Click(object sender, EventArgs e) {
+			Close();
+		}
+
+		private void openMenuItem_Click(object sender, EventArgs e) {
+			if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+				csvgrid.Columns.Clear();
+				csvgrid.Rows.Clear();
+				string file = File.ReadAllText(openFileDialog1.FileName);
+				StringReader reader = new StringReader(file);
+				CsvParser csv = new CsvParser();
+				string[][] dataStrings = csv.Parse(reader);
+				filename = openFileDialog1.FileName;
+				populateGrid(dataStrings);
+				this.Focus();
+			}
+		}
+
+		private void populateGrid(string[][] dataStrings) {
+			csvgrid.RowsDefaultCellStyle.BackColor = Color.Azure;
+			csvgrid.AlternatingRowsDefaultCellStyle.BackColor = Color.White;
+			csvgrid.CellBorderStyle = DataGridViewCellBorderStyle.None;
+			if (dataStrings.Length > 1) {
+				for (uint i = 0; i < dataStrings[0].Length; i++) {
+					csvgrid.Columns.Add(dataStrings[0][i], firstRowHeader?dataStrings[0][i]:(i+1)+"");
+				}
+				//int start
+				for (uint i = (uint)(firstRowHeader?1:0); i < dataStrings.Length; i++) {
+					csvgrid.Rows.Add(dataStrings[i]);
+				}
+				csvgrid.AutoResizeColumns();
+				this.Text = "CSV Reader - " + filename;
+				statustext.Text = (dataStrings.Length - 1) + " Rows";
+			}
+			else {
+				for (uint i = 0; i < dataStrings[0].Length; i++) {
+					csvgrid.Columns.Add(dataStrings[0][i], (i + 1) +  "");
+				}
+				csvgrid.Rows.Add(dataStrings[0]);
+				csvgrid.AutoResizeColumns();
+				this.Text = "CSV Reader - " + filename;
+				statustext.Text = (dataStrings[0].Length) + " Columns, 1 Row";
+			}
+			csvgrid.Refresh();
+			csvgrid.CurrentCell = csvgrid[0, 0];
+			gridDirty = false;
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+			AboutBox1 a = new AboutBox1();
+			a.ShowDialog();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			if (gridDirty) {
+				MessageBox.Show("Your changes will not be saved", "Read only", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private void csvgrid_CellValueChanged(object sender, DataGridViewCellEventArgs e) {
+			gridDirty = true;
+			statustext.Text = "Changed";
+		}
+
+		private void closeButton_Click(object sender, EventArgs e) {
+			Close();
+		}
+
+		private void Form1_Shown(object sender, EventArgs e) {
+			if (Environment.GetCommandLineArgs().Length == 1) return;
+			string[] args = Environment.GetCommandLineArgs();
+
+			string file = File.ReadAllText(args[1]);
+			StringReader reader = new StringReader(file);
+			CsvParser csv = new CsvParser();
+			string[][] dataStrings = csv.Parse(reader);
+			filename = args[1];
+			headerButton.Checked = firstRowHeader;
+			headerOptionMenuItem.Checked = firstRowHeader;
+			populateGrid(dataStrings);
+			this.Focus();
+		}
+
+		private void csvgrid_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.All;
+			else
+				e.Effect = DragDropEffects.None;
+			statustext.Text = "DragEnter";
+		}
+
+		private void csvgrid_DragDrop(object sender, DragEventArgs e) {
+			CsvParser csv = new CsvParser();
+			//StringReader sr = new StringReader(e.Data.GetData(DataFormats.Text).ToString());
+			csvgrid.Columns.Clear();
+			csvgrid.Rows.Clear();
+			string filename = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+			//string file = File.ReadAllText(e.Data.GetData(DataFormats.FileDrop));
+			string file = File.ReadAllText(filename);
+			StringReader reader = new StringReader(file);
+			string[][] dataStrings = csv.Parse(reader);
+			filename = openFileDialog1.FileName;
+			populateGrid(dataStrings);
+			this.Focus();
+		}
+
+		private void headerButton_Click(object sender, EventArgs e) {
+			
+		}
+
+		private void useFirstRowAsHeadersToolStripMenuItem_Click(object sender, EventArgs e) {
+
+		}
+
+		private void headerButton_Click_1(object sender, EventArgs e) {
+			if (headerButton.Checked) {
+				headerButton.Checked = false;
+				headerOptionMenuItem.Checked = false;
+				firstRowHeader = false;
+			}
+			else {
+				headerButton.Checked = true;
+				headerOptionMenuItem.Checked = true;
+				firstRowHeader = true;
+			}
+		}
+
+	}
+}
